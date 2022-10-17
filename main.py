5877d9c826f3344ca5c47eae67fcac822b87dc80
unknown <Administrator@SERVER1.intercall.al>
2022-10-17 10:53:38 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+#
+
+#
+# mylist = "7 77 55 2345"
+# my_sec_list = mylist.split()
+#
+# print(max(my_sec_list))
+
+# class Myworld:
+#     x = 4
+#     y = 7
+#     z = x + y
+#
+# p1 = Myworld()
+# print(p1.z)
+
+
+# class Myprogram:
+#     var1 = int(input("enter your age"))
+#     var2 = int(input("enyert your nymber"))
+#     if var1 > var2:
+#         print('bravo')
+#     else:
+#         print("gabim")
+#     var3 = "kjo eshttt"
+#
+# p1 = Myprogram()
+# print(p1.var3)
+
+# class Myprogram:
+#     def __int__(self, name, age, vendlindje):
+#         self.name = name
+#         self.age = age
+#         self.vendlidje = vendlindje
+#
+#     def myfunc(self):
+#         print('Emri: ' + self.name)
+# name, age, venflindje = ["ego", 22, "tiran"]
+# p1 = Myprogram(name, age, vendlindje)
+# p1.myfunc()
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     continue
+#   print(x)
+
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
